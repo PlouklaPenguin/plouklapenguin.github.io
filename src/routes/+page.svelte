9063753f76ad0b33f1646edaa64f91efdd77aea4
Sam Robinson <62719122+PlouklaPenguin@users.noreply.github.com>
2023-02-02 21:05:59 -0800

    page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="">
+    <h1>My stuff</h1>
+    <li>
+        <u><a href="/stravabot">StravaBot</a></u>
+        <u><a href="/1600">1600 Website</a></u>
+    </li>
+</div>
+
+
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
---
+++ src/routes/stravabot/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    let catpath = '/src/lib/cat.jpg'
+    let discordpath = '/src/lib/discord.jpg'
+</script>
+
+<div class="grid place-content-center w-screen h-screen text-white bg-black">
+    <h1 class="">stravabot</h1>
+</div>
+
+<div class="grid grid-cols-1 py-16 w-screen">
+    <h1 class="pb-16 text-2xl text-center">
+        Do you enjoy? strava. Or do you just want to matisse your discord up a bit?
+    </h1>
+    <div class="grid grid-cols-2 gap-4 px-8 py-4 bg-slate-600">
+        <p class="place-self-center">
+            Central to the matisse philosiphy is a hate of dogs if you like dogs then you are not
+            you like the molasses that grows in windter. Durn it! If matiise is then matisse was and
+            if matisse is matisse then what is a dog if not evil?
+        </p>
+
+        <img class="place-self-center w-64 h-64 rounded" src={catpath} alt="cat" />
+    </div>
+    <div class="grid grid-cols-2 gap-4 px-8 py-4 bg-slate-200">
+        <img class="place-self-center w-64 h-64 rounded" src={discordpath} alt="sowing discords" />
+
+        <p class="place-self-center">
+            Is matisse not enough for you in yuo'er pitiful life? Well Darl has an answer:
+            stravabot. You can add it to any discord and, once connected to your account, will post
+            all activities in your club to that discord. Yay!
+        </p>
+    </div>
+</div>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
